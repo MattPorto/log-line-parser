25ef60b0c04e0b1f82386ceac41b67325577ef41
Matheus Porto Pereira <matheus.pereira11@gmail.com>
2021-04-17 12:37:03 -0300

    test(file-handler): add contexts plus clean code

--- spec/lib/file_handler_spec.rb
+++ spec/lib/file_handler_spec.rb
@@ -4,13 +4,17 @@ require 'spec_helper'
 require 'file_handler'
 
 describe FileHandler do
-  it 'init' do
-    log_path = 'spec/fixtures/basic.log'
-    options = { file_path: log_path }
+  let(:basic_log_path) { 'spec/fixtures/basic.log' }
+  subject(:options) { { file_path: log_path } }
 
-    result = described_class.new(options).call
+  context 'data collect' do
+    let(:log_path) { basic_log_path }
 
-    expect(result.keys).to eq %w[/help_page/1 /contact /home]
-    expect(result['/help_page/1'][:hits]).to eq 1
+    it 'get hits' do
+      result = described_class.new(options).call
+
+      expect(result.keys).to eq %w[/help_page/1 /contact /home]
+      expect(result['/help_page/1'][:hits]).to eq 1
+    end
   end
 end
