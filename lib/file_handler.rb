0c49bf0a26a075ed948c1c824714efb9c9577d34
Matheus Porto Pereira <matheus.pereira11@gmail.com>
2021-04-17 13:27:55 -0300

    refactor(file-handler): add errors treatment plus helper methods

--- lib/file_handler.rb
+++ lib/file_handler.rb
@@ -4,18 +4,57 @@
 class FileHandler
   def initialize(options)
     @file_path = options[:file_path]
+    @result = {}
   end
 
   def call
-    file = File.read @file_path
-    result = {}
+    file = file_accessor
+    extract_content file unless file_has_errors
+    @result
+  end
 
-    file.each_line do |line|
-      path, _ip = line.split(' ')
-      result[path] ||= { path: path, hits: 0, uniques: 0 }
-      result[path][:hits] += 1
-    end
+  private
 
-    result
+  def file_by_path
+    File.read @file_path if File.exist? @file_path
+  end
+
+  def file_accessor
+    file = file_by_path
+    file.nil? ? empty_file_error : file
+  end
+
+  def extract_content(file)
+    file.each_line { |line| extract_data line if valid_data line }
+  end
+
+  def valid_data(line)
+    # @ToDo: add line index arg, so system can specify which line is empty;
+    #   add also malformed log lines checker
+    line.empty? ? empty_line_error : true
+  end
+
+  def extract_data(line)
+    path, _ip = line.split(' ')
+    @result[path] ||= { path: path, hits: 0, uniques: 0 }
+    @result[path][:hits] += 1
+  end
+
+  def empty_file_error
+    add_error_base
+    @result[:errors].push({ message: 'Please provide a file to be analysed.' })
+  end
+
+  def empty_line_error
+    add_error_base
+    @result[:errors].push({ message: 'File have empty lines.' })
+  end
+
+  def add_error_base
+    @result[:errors] ||= []
+  end
+
+  def file_has_errors
+    @result.keys.include? :errors
   end
 end
--- spec/lib/file_handler_spec.rb
+++ spec/lib/file_handler_spec.rb
@@ -6,15 +6,27 @@ require 'file_handler'
 describe FileHandler do
   let(:basic_log_path) { 'spec/fixtures/basic.log' }
   subject(:options) { { file_path: log_path } }
+  subject(:call_scope) { described_class.new(options).call }
 
   context 'data collect' do
     let(:log_path) { basic_log_path }
 
     it 'get hits' do
-      result = described_class.new(options).call
+      result = call_scope
 
       expect(result.keys).to eq %w[/help_page/1 /contact /home]
       expect(result['/help_page/1'][:hits]).to eq 1
+    end
+  end
+
+  context 'validations' do
+    let(:log_path) { '' }
+
+    it 'no filepath' do
+      result = call_scope
+
+      expect(result.keys).to include :errors
+      expect(result[:errors][0][:message]).to eq 'Please provide a file to be analysed.'
     end
   end
 end
